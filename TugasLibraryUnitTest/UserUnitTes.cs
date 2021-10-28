cfd2e66931c146a0c19c2810c22f9a1c4eeeffbe
Dika adit <adityahos107@gmail.com>
2021-10-28 11:34:06 +0700

    Add project files.

---
+++ TugasLibraryUnitTest/UserUnitTes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class UserUnitTes
+    {
+        private User user;
+
+        [SetUp]
+        public void Init() 
+        {
+             user = new User();
+        }
+        [Test]
+        public void IsValidUserTest() 
+        {
+            var userName = "admin";
+            var password = "admin";
+            Assert.IsTrue(user.IsValidUser(userName,password));
+        }
+    }
+}
